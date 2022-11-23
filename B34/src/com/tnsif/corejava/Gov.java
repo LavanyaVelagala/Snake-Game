27e5b064fd5f4483dea51b6fbff73e026ed3bf5c
nagar <nagar@LAPTOP-1ET41LHS>
2022-11-23 13:11:36 +0530

    interface

---
+++ B34/src/com/tnsif/corejava/Gov.java
@@ -0,0 +1,8 @@
+package com.tnsif.corejava;
+interface  Gov{
+	public void GST();
+	}
+	
+	
+
+	
---
+++ B34/src/com/tnsif/corejava/Mobile.java
@@ -0,0 +1,12 @@
+package com.tnsif.corejava;
+
+class Mobile implements Gov{
+		public void GST() {
+			System.out.println("4.3");
+		}
+		
+	public static void main(String[] args) {
+		Gov O = new Mobile();
+		O.GST();
+	}
+		}
