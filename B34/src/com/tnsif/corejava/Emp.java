11768d9c4f9b3826cfc9e2920970046dff02f19c
nagar <nagar@LAPTOP-1ET41LHS>
2022-11-23 13:52:13 +0530

    Static Instance variable

--- B34/src/com/tnsif/corejava/Emp.java
+++ B34/src/com/tnsif/corejava/Emp.java
@@ -9,9 +9,9 @@ public class Emp{
 public static void main(String args[]){  
      Emp O =new Emp();      
      System.out.println (O.a);
-     
+     System.out.println(b);
      O.print();
-     System.out.println(O.b);
+     
 }
 }
 
